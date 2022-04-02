290a6b1d243ffba9ffe6ef45b380ca56437e3711
BouBou <69978359+BouletteDeSheet@users.noreply.github.com>
2022-04-02 19:57:58 +0200

    update

--- commands/Misc/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "ping",
-    aliases: [],
-    description: "",
-execute: async (client, message, args) => {
-    message.channel.send(`:ping_pong: • ${message.author} pong!`)
-    }
-}
